217ab325fde538295eb84e5cf2a8c625763cd933
KMO <kmo.web.dev@gmail.com>
2025-09-07 00:49:58 -0400

    fix: configure environment variables for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({ mode }) => {
   // Cargar variables de entorno
   const env = loadEnv(mode, process.cwd(), '')
   
